4e40b75d4726386386139d38bdf0c05bfe178a49
sachin <sachincodingkart@gmail.com>
2019-12-19 19:28:09 +0530

    Initial commit

--- index.js
+++ index.js
@@ -101,7 +101,7 @@ app.post('/login',(req, res) => {
   console.log(password);
   if (username && password) {  
 
-     let sql = 'SELECT * FROM user_data WHERE email='+username+' AND password ='+password+'';
+     let sql =  "SELECT * FROM user_data WHERE id="+username+"";
      let query = conn.query(sql, (err, results) => {
         if (err) {
           console.log(err.stack)
