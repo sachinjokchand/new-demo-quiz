d20f81d8b4dd929dfe05a1a0fb8cb26695f7a33a
sachin <sachincodingkart@gmail.com>
2019-12-19 21:08:08 +0530

    Initial commit

--- index.js
+++ index.js
@@ -279,7 +279,7 @@ app.post('/submit_test',(req, res) => {
           console.log(results.rows)
            console.log(results.rows[0].answer);
            console.log(answer[i])
-          if( results.rows[0].answer.trim() === answer[i].trim() )
+          if( answer[i] == results.rows[0].answer )
           {
             correct++;
           console.log(results.rows[0].answer);
