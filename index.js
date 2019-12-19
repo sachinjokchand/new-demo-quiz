8d5282369345f7df3c472958e3ca4f7226617a21
sachin <sachincodingkart@gmail.com>
2019-12-19 14:17:21 +0530

    Initial commit

--- index.js
+++ index.js
@@ -253,7 +253,7 @@ app.post('/submit_test',(req, res) => {
   for (var i = 0; i <total.length; i++) {
    
    var question_id = req.body.question.i;
-   var answer    = req.body.i;
+   var answer    = req.body.anwere.i;
    console.log(question_id);
    console.log(answer);
 
