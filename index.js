8b780965fc624239cd9e49b0481c7e4e04c756a5
sachin <sachincodingkart@gmail.com>
2019-12-20 16:27:30 +0530

    Initial commit

--- index.js
+++ index.js
@@ -274,21 +274,9 @@ app.post('/submit_test',(req, res) => {
          let sql = "SELECT * FROM quiz WHERE id='"+question_id[i]+"' AND answer='"+answer[i]+"'";
         let query = conn.query(sql, (err, results) => {
            console.log(results)
-            console.log(i)
-          console.log(results.rows)
-        if (results.rows.length > 0) {
-          
-          console.log(results.rows);
-        }
-        else {
-
-           console.log(err);
-          console.log(results.rows);
-           console.log('aaaaaaaaa');
-            console.log(results.rows.length > 0);
-             console.log(results.rows.length);
-          res.send('Incorrect Username and/or Password!');
-        }
+            console.log(err)
+          // console.log(results.rows)
+       
       })
      }
 
